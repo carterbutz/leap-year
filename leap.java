44f722fe48e08ee8763338b3db5d6302f30d60b3
carterbutz <56846076+carterbutz@users.noreply.github.com>
2019-10-21 17:31:31 -0400

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner*;
+public class LeapYear {
+	public static void main(String[] args) {
+	int year;
+
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter a year.");
+
+	if (year% 4 == 0 && 100 != 0 || year % 400 == 0) {
+			System.out.printf("%d is a leap year")
+
+	}
+
+
+	}
+
+}
